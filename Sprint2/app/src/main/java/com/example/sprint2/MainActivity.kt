287e63f6b079b5d65835921ddd46c07b9047d2bb
emilio-stack <84351829+emilio-stack@users.noreply.github.com>
2022-05-21 23:38:41 -0600

    adding sprint 2 files

---
+++ Sprint2/app/src/main/java/com/example/sprint2/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.example.sprint2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**********************************************************
+ * GAME
+ * A class to practice flash cards
+ *********************************************************/
+class Game {
+
+    // Initialize Member Variables
+    private var practiceNum = 0
+    private var operand = ""
+    private var generatedNum = 0
+    private var answer = 0
+    private var numsAlreadyUsed = mutableSetOf<Int>(0)
+    private var currentProblem = arrayOf<String>("", "", "")
+
+    // Initialize Method
+    /**********************************************************
+     * A function to set the operand type.
+     *********************************************************/
+    fun setOperand(o : String) {
+        when (o) {
+            "x" -> operand = "x"
+            "/" -> operand = "/"
+            "+" -> operand = "+"
+            "-" -> operand = "-"
+        }
+    }
+
+    /**********************************************************
+     * A function to get the operand type.
+     *********************************************************/
+    fun getOperand() : String {
+        return operand
+    }
+
+    /**********************************************************
+     * A function to set the practice number. Number must be
+     * greater than 0 and less than 12
+     *********************************************************/
+    fun setPracticeNum(num : Int) {
+        if (num in 1..12)
+            practiceNum = num
+    }
+
+    /**********************************************************
+     * A function to get the practice number.
+     *********************************************************/
+    fun getPracticeNum(): Int {
+        return practiceNum
+    }
+
+    /**********************************************************
+     * A function to generate a new problem
+     *********************************************************/
+    private fun generateProblem() {
+        while (generatedNum in numsAlreadyUsed) {
+            generatedNum = (1..12).random()
+        }
+        when (operand) {
+            "x" -> answer = practiceNum * generatedNum
+            "/" -> answer = practiceNum / generatedNum
+            "+" -> answer = practiceNum + generatedNum
+            "-" -> answer = practiceNum - generatedNum
+        }
+        numsAlreadyUsed.add(generatedNum)
+        currentProblem[0] = practiceNum.toString()
+        currentProblem[1] = operand
+        currentProblem[2] = generatedNum.toString()
+    }
+
+    /**********************************************************
+     * A function to get the current problem.
+     *********************************************************/
+    fun getCurrentProblem() : String {
+        return "${practiceNum} ${operand} ${generatedNum} = "
+    }
+
+    /**********************************************************
+     * A function to return a new problem
+     *********************************************************/
+    fun getNewProblem() : String {
+        generateProblem()
+        return getCurrentProblem()
+    }
+
+    /**********************************************************
+     * A function to check if an user answer is correct
+     *********************************************************/
+    fun isCorrect(uAnswer : Int) : Boolean {
+        return uAnswer == answer
+    }
+
+    /**********************************************************
+     * A function to return the correct answer
+     *********************************************************/
+    fun getAnswer() : String {
+        return answer.toString()
+    }
+
+
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
